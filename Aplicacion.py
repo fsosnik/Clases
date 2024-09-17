191eb761b670496098e7143dc3d3bb96ca95b828
gefe69 <52205979+fsosnik@users.noreply.github.com>
2024-09-16 22:18:39 -0300

    Add files via upload

---
+++ Aplicacion.py
@@ -0,0 +1,12 @@
+#Esto es un comentario...
+print ("Hola Mundo!")
+print ("Hola Python")
+
+nombre = input("Ingrese su nombre: ")
+print("Hola  " + nombre)
+
+numero1 = 20
+numero2 = 43
+
+promedio = (numero1 + numero2) / 2
+print("El promedio es: " , promedio)
